ab8a6454cf896fed4a7631ac894667b053412217
mdfiroz811315 <55021994+mdfiroz811315@users.noreply.github.com>
2022-07-18 16:35:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+//const bodyParser = require("body-parser");
+//const cors = require("cors");
 const exerciseRoute = require("./routes/exercise");
 const userRoute = require("./routes/users");
 //const mongoose = require("mongoose");
@@ -9,8 +9,8 @@ const userRoute = require("./routes/users");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
+//app.use(cors());
+//app.use(bodyParser.json());
 /*
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
